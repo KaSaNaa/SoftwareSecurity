754cb177c7116e7e385b876c8bcab6704d4fb85e
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-14 23:17:38 +0530

    deps configured (uninstalled unused deps)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,10 @@
-import Bot from "./components/Bot";
-import styled from "styled-components";
+
 
 const App = () => {
   return (
-    <BotWrapper>
-      <Bot/>
-    </BotWrapper>
+    <>
+      <h1>APP</h1>
+    </>
   );
 };
 export default App;
-
-const BotWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-color: black;
-  background-color: darkslategrey;
-`;
