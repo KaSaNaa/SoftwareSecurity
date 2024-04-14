50d6d668779de22df11a01ec06ad70373d22c36b
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-14 23:41:01 +0530

    Refactor MongoDB connection in server/config/db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/myapp", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect("mongodb://localhost:27017");
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
