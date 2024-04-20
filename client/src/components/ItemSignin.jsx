88f8a806c6df33ea234e9517ac7baf109e7cd9bf
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-20 20:51:34 +0530

    Refactor sign-in and sign-up forms in ItemSignin.jsx and ItemSignup.jsx

--- client/src/components/ItemSignin.jsx
+++ client/src/components/ItemSignin.jsx
@@ -1,37 +1,48 @@
+import { Link } from "react-router-dom";
 import "./ItemSignup.css";
-import { Form, Link } from "react-router-dom";
+
 export default function ItemSignin() {
   return (
     <div className="main-li-container">
       <h1 className="signup-head">Sign In</h1>
-      <Form>
-        <ul>
-          <li className="item-list">
-            <label className="class-label">Email</label>
-            <input className="class-input" type="email"></input>
-          </li>
-          <li className="item-list">
-            <label className="class-label">Password</label>
-            <input className="class-input" type="password"></input>
-          </li>
-
-          <li className="item-list">
-            <button className="signup-button">Sign In</button>
-          </li>
-          <div className="decoy-div">
-            <li className="item-list">
-              <p className="signin-text">
-                Don&apos;t Have an account?{" "}
-                <button className="signin-link">
-                  <span>
-                    <Link to={"/signin"}>Sign up</Link>
-                  </span>
-                </button>
-              </p>
-            </li>
-          </div>
-        </ul>
-      </Form>
+      <form>
+        <div className="item-list">
+          <label className="class-label" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="class-input"
+            type="email"
+            id="email"
+            name="email"
+            autoFocus
+          />
+        </div>
+        <div className="item-list">
+          <label className="class-label" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="class-input"
+            type="password"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="item-list">
+          <button className="signup-button" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div className="decoy-div">
+          <p className="signin-text">
+            Don&apos;t Have an account?{" "}
+            <Link className="signin-link" to="/signup">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </form>
     </div>
   );
 }
--- client/src/components/ItemSignup.jsx
+++ client/src/components/ItemSignup.jsx
@@ -1,47 +1,82 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import './ItemSignup.css';
+import { Link } from "react-router-dom";
+import "./ItemSignup.css";
 
 export default function ItemSignup() {
   return (
-    <div className='main-li-container'>
-      <h1 className='signup-head'>Sign In</h1>
-      <ul>
-        <li className='item-list'>
-          <label className='class-label'>Name</label>
-          <input className='class-input' type='text'></input>
-        </li>
-        <li className='item-list'>
-          <label className='class-label'>NIC Number</label>
-          <input className='class-input' type='text'></input>
-        </li>
-        <li className='item-list'>
-          <label className='class-label'>Account Number</label>
-          <input className='class-input' type='text'></input>
-        </li>
-        <li className='item-list'>
-          <label className='class-label'>Email</label>
-          <input className='class-input' type='email'></input>
-        </li>
-        <li className='item-list'>
-          <label className='class-label'>Password</label>
-          <input className='class-input' type='password'></input>
-        </li>
-        <li className='item-list'>
-          <label className='class-label'>Confirm Password</label>
-          <input className='class-input' type='password'></input>
-        </li>
-        
-        <li className='item-list'>
-          <button className='signup-button'>Sign Up</button>
-        </li>
-        <div className="decoy-div">
-        <li className='item-list'>
-          <p className='signin-text'>Already Have an account? <button className='signin-link'>Sign in</button>
-          </p>
-        </li>
+    <div className="main-li-container">
+      <h1 className="signup-head">Sign In</h1>
+      <form>
+        <div className="item-list">
+          <label className="class-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="class-input"
+            type="text"
+            id="name"
+            name="name"
+            autoFocus
+          />
         </div>
-      </ul>
+        <div className="item-list">
+          <label className="class-label" htmlFor="nic">
+            NIC Number
+          </label>
+          <input className="class-input" type="text" id="nic" name="nic" />
+        </div>
+        <div className="item-list">
+          <label className="class-label" htmlFor="account">
+            Account Number
+          </label>
+          <input
+            className="class-input"
+            type="text"
+            id="account"
+            name="account"
+          />
+        </div>
+        <div className="item-list">
+          <label className="class-label" htmlFor="email">
+            Email
+          </label>
+          <input className="class-input" type="email" id="email" name="email" />
+        </div>
+        <div className="item-list">
+          <label className="class-label" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="class-input"
+            type="password"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="item-list">
+          <label className="class-label" htmlFor="confirmPassword">
+            Confirm Password
+          </label>
+          <input
+            className="class-input"
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+          />
+        </div>
+        <div className="item-list">
+          <button className="signup-button" type="submit">
+            Sign Up
+          </button>
+        </div>
+        <div className="decoy-div">
+          <p className="signin-text">
+            Already have an account??{" "}
+            <Link className="signin-link" to="/signin">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </form>
     </div>
   );
 }
